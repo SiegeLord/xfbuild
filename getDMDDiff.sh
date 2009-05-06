06e8bcb10f8739c5ec763173039cb3980d043365
Tomasz Stachowiak <h3@team0xf.com>
2009-05-07 00:45:03 +0200

    linux-friendly patching script

--- getDMDDiff.sh
+++ getDMDDiff.sh
@@ -1,1 +1,5 @@
-diff -u -x "*.exe" -x "*.swp" -x "*.map" -x win32.mak origDMD dmd | grep -v "^Only in dmd:" | grep -v "^Common subdirectories:"
+diff -d --strip-trailing-cr --ignore-tab-expansion -x "*.obj" -x "*.exe" -x "*.swp" -x "*.map" -x win32.mak -rpu origDMD dmd | grep -v "^Only in dmd:" | grep -v "^Common subdirectories:" > dmdDiff.patch
+rm patchDMD.sh
+touch patchDMD.sh
+grep '+++ dmd/.*' dmdDiff.patch | sed -e 's/.*+++ \([^	]*\).*/dos2unix \1/' >> patchDMD.sh
+echo "patch -p0 -ui dmdDiff.patch" >> patchDMD.sh
--- patchDMD.sh
+++ patchDMD.sh
@@ -1,1 +1,4 @@
+dos2unix dmd/import.c
+dos2unix dmd/mars.c
+dos2unix dmd/mars.h
 patch -p0 -ui dmdDiff.patch

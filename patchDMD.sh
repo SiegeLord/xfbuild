8e1b0ab28f6bc685f3cc3bd7809e4157110d9ff8
Tomasz Stachowiak <h3@team0xf.com>
2009-05-17 18:52:07 +0200

    removed the header generation changes from the diff; headers are doomed anyway

--- patchDMD.sh
+++ patchDMD.sh
@@ -1,11 +1,4 @@
-dos2unix dmd/attrib.c
-dos2unix dmd/class.c
-dos2unix dmd/declaration.c
-dos2unix dmd/expression.c
-dos2unix dmd/func.c
-dos2unix dmd/hdrgen.h
 dos2unix dmd/import.c
 dos2unix dmd/mars.c
 dos2unix dmd/mars.h
-dos2unix dmd/struct.c
 patch -p0 -ui dmdDiff.patch
